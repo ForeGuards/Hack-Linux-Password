2fab3a18fe884be53d4d9f7f9f3ad230359f6016
WhiteHat-Hacking <35286609+WhiteHat-Hacking@users.noreply.github.com>
2018-01-15 23:14:25 +0100

    Update passwort-crack.py

--- passwort-crack.py
+++ passwort-crack.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-# Show the user password hash: cat /etc/shadow
+# give the following command in the terminal to show you the hash from the users: sudo cat /etc/shadow
+
 # For example in this case: username:$6$bVtoNJr/GHPs7R$i7Ip5frU9wEfPfGhFGE2w6CkdTpL21zQ2zRbZyR51KkFilzOf8prHxhxpiDzuxRhS8/TZyQ04t25hDXOajIl7.
 
-# The algoritmus in this case are: $6$ = SHA512
+# The algorithmus in this case are: $6$ = SHA512
 # The password-salt in this case are: $6$bVtoNJr/GHPs7R$
 
 password_hash = "$6$bVtoNJr/GHPs7R$i7Ip5frU9wEfPfGhFGE2w6CkdTpL21zQ2zRbZyR51KkFilzOf8prHxhxpiDzuxRhS8/TZyQ04t25hDXOajIl7."
